cdf6045e5f636b65490310dd416b925901263ba1
Carlos070508 <arturovelasquez1555@gamil.com>
2025-09-03 14:44:09 -0500

    1

---
+++ Operaciones_aritmeticas.py
@@ -0,0 +1,26 @@
+
+a = 2*(4*(7+4*(5*3-9))-3*(40-8))
+print (f"Ejemplo 2= {a}")
+
+#ejercicio 3
+c= 3*(4+(2*(3+5))+2)
+print (f"Ejemplo 3= {c}")
+
+#ejercicio 4
+d= 2*(4*(7+4*6)-3*32)
+print (f"Ejemplo 4= {d}")
+
+
+import math
+#ejercicio 5   
+# Numerador
+numerador = (3 * (5**2)) + (4 * (7 + 8)) - (2**(3*2))
+
+# Denominador
+denominador = (9 / 3) ** ((4 + 2) * 2)
+
+# Fracción
+resultado = numerador / denominador
+
+print("El resultado es:", resultado)
+
