08b8c586781a760db3794ab6aab292be63727f51
Carlos070508 <arturovelasquez1555@gamil.com>
2025-09-03 23:03:08 -0500

    entrega

---
+++ Operadores_De_Comparacion.py
@@ -0,0 +1,15 @@
+Num1 = 5
+Num2 = 10
+
+#Igualdad
+print(Num1 == Num2)
+#difrerente
+print(Num1 != Num2)
+#Mayor que
+print(Num1 > Num2)
+#Menor que
+print(Num1 < Num2)
+#Mayor o igual que
+print(Num1 >= Num2)
+
+
